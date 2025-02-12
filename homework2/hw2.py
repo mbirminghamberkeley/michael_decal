ac798b128243929884bae57e9d32e13437b6ab2b
Michael Birmingham <michael_birmingham@berkeley.edu>
2025-02-11 19:57:57 -0800

    Adding homework 2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,29 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+#you could use cd ../.. to get directly to the desktop
+
 # 2) What does ~/ mean?
+#this represents the home directory.
+
 # 3) What's the difference between and abolute and a relative path in your terminal?
+#an absolute path starts from the home directory no matter what, while a relative path starts from where you currently are.
+
 # 4) What command would bring you back to your home directory?
+#you could just run cd by itself to get back to the home directory
+
 # 5) If you called rm ./ in your current working directory, what would it do? 
+#it would cause an error because it couldn't delete a directory by default.
+
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+#git add basically prepares a file to be committed, git commit saves the files to the local repository, and git push sends it to a remote repository (github in our case)
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+#it would show you which changes are being committed to the repository. Essentially, it's showing you what you're going to be putting in your repository.
+
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+#probably just setting up github, since there were a few differences on my computer vs. the tutorial's. I mainly just read error messages and tried to decipher them, which seems to have worked.
+
 # 9) How does cloning a repository differ from pulling from a repository?
+#git clone copies an entire repository into your local repository, while git pull updates an existing repository
+
 # 10) Tell me a fun fact!
+#elephants can purr
